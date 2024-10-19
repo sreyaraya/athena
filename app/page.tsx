d25fcbebdb15a8a79bb547989b488a3bf0c7ed51
Aarnav Sanghvi <aarnavsanghvi890@gmail.com>
2024-10-19 02:40:36 -0700

    I COOKED THE UI YIUPEEEEEEE

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 'use client'
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image";
+import soundthingy from "../components/nounsound.svg"
+import waves from "../components/waveswhite.png"
+import speak from "../components/speak.svg"
 
 
 export default function Home() {
@@ -9,30 +13,95 @@ export default function Home() {
   return (
     <div className="text-white font-f ">
       <div className="flex flex-col items-center gap-10 text-center">
-          <div className="mt-16 text-8xl ">
-            Athena
-          </div>
+          <div className="mt-12 text-8xl flex gap-6">
+            <div>Athena</div>
+            <div><Image src={soundthingy} alt={"just get better wifi dawg"} >
 
-          <div className="mt-4 text-4xl ">
-          <span className=" pl-2 pr-2 ">Real-time</span>Voice Analysis with Industry-Specific Feedback 
+</Image></div>
           </div>
+          
+          <div className="scale-[1.3] opacity-80 -mt-14">
+      <Image
+        src={waves}
+        alt="Waves"
+        
+      />
+    </div>
 
-          <div>
-            <button onClick={() => signIn()} className="p-2 text-white duration-200  rounded-sm hover:scale-110 active:scale-90">
+          <div className="-mt-2 text-4xl">
+            <div className="flex gap-4 flex-col min-w-max">
+
+              <div className=" min-h-[50px] flex justify-start">
+                    <span className=" animate-first text-transparent bg-clip-text bg-gradient-to-r from-gray-50 to-gray-100 text-5xl">Real-Time</span>
+                    <span className="animate-second text-transparent bg-clip-text bg-gradient-to-r from-gray-50 to-gray-100 text-5xl"> Career-Tailored</span>
+                    <span className="animate-third text-transparent bg-clip-text bg-gradient-to-r from-gray-50 to-gray-100 text-5xl">Uniquely-Detailed</span>
+
+              </div>
+
+              <div className="self-end -mt-4">
+                AI Interview Assistant with Computed Prep Feedback
+                </div>
+
+
+            </div>
+         
+          
+
+          <div className="mt-12 flex justify-start gap-10 text-lg ">
+            <button onClick={() => signIn()} className="p-2 hover:bg-purple-400 text-white  rounded-sm duration-200 border border-white hover:scale-110 active:scale-90">
               <Link href={'/auth'}>
-              Get Started
+              Sign Up
+              </Link>
+              
+            </button>
+
+            <button onClick={() => signIn()} className="p-2 hover:bg-purple-400 text-white  rounded-sm duration-200 border border-white  hover:scale-110 active:scale-90">
+              <Link href={'/auth'}>
+              Log In
               </Link>
               
             </button>
           </div>
+          
+          </div>
 
-          <div className="flex self-start w-full mt-96 bg-white h-64">
-            <div className="flex flex-col justify-center w-1/6 h-full text-white bg-clear">
+          <div className="flex gap-8 justify-center w-full mt-10 bg-darkpurple h-64">
+           
+            <div className="bg-black flex-col flex">
+              <div className="">
+              <Image width={"200"} src={speak}>
+
+                </Image>
+                </div>
+                <div>
+                  Tell Athena about your upcoming interview
+                </div>
+            </div>
+
+            <div className="bg-red flex flex-col">
+              <div>
+                  Athena will give you questions and listen critically to analyze your answers
+                </div>
+              </div>
+
+              <div className="bg-red flex flex-col">
+              <div>
+                  Get a post interview debrief with our original PrepFeedback Score
+                </div>
+              </div>
+
+
+
+          </div>
+          
+
+          <div className="flex self-start w-full mt-96 bg-darkpurple h-64">
+            <div className="flex flex-col justify-center w-1/6 h-full text-white bg-darkpurple">
               <p className="m-6 text-5xl ">
                 How does it work?
               </p>
             </div>
-            <div className="flex flex-col justify-center w-5/6 h-full p-20 text-3xl text-white bg-white">
+            <div className="flex flex-col justify-center w-5/6 h-full p-20 text-3xl text-white bg-darkpurple">
               Siteify allows users to create beautiful ai-enhanced portfolio websites, automatically integrated with their projects, all with a few simple clicks! 
             </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,38 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors:{
+        darkpurple: "#2f0f47",
+      },
       animation: {
+        "fade-in-fast": "fade-in 0.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both",
+      
+        "puff-in-center": "puff-in-center 0.5s cubic-bezier(.44,.12,.11,.86)  both",
+        "fade-in-bottom": "fade-in-bottom 0.5s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both",
+        "nothing": "nothing 1s linear both",
+        "highlight": "highlight 0.5s cubic-bezier(1,.01,.5,1.03) both ",
+        "scale-in-left": "scale-in-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
+        "slide-in-blurred-left": "slide-in-blurred-left 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000)   both",
+        mypulse: "pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;",
+        spinSlow: 'spin 3s linear infinite',
+        goodPulse: "a 0.6s cubic-bezier(0.4, 0, 0.6, 1) infinite;",
+        "first": "text 9s cubic-bezier(0.250, 0.460, 0.450, 0.940)  infinite both",
+        "second": "text 9s cubic-bezier(0.250, 0.460, 0.450, 0.940) 3s infinite both",
+        "third": "text 9s cubic-bezier(0.250, 0.460, 0.450, 0.940) 6s infinite both",      
+        "firsta": "emoji 9s cubic-bezier(0.250, 0.460, 0.450, 0.940)  infinite both",
+        "seconda": "emoji 9s cubic-bezier(0.250, 0.460, 0.450, 0.940) 3s infinite both",
+        "thirda": "emoji 9s cubic-bezier(0.250, 0.460, 0.450, 0.940) 6s infinite both",
+        "heartbeat": "heartbeat 1.5s ease  infinite both",
+        "vibrate-1": "vibrate-1 0.4s linear  infinite both",
+        "pulsate-fwd": "pulsate-fwd 0.5s ease  infinite both",
+        "rotate-emoji": "rotate-emoji 0.8s ease  infinite both",
         "scale-in-center": "scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
         "text-focus-in": "text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530)   both",
         "text-focus-in-with-delay": "text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) 1s  both",
-        "focus-in-expand-fwd": "focus-in-expand-fwd 0.8s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both"
+        "focus-in-expand-fwd": "focus-in-expand-fwd 0.8s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
+ 
       },
+
       fontFamily: {
         'f': ['DM Sans', 'serif']
         ,
@@ -51,7 +77,334 @@ module.exports = {
                 opacity: "1"
             }
         }
+    },
+
+    keyframes: {
+      "fade-in": {
+        "0%": {
+            opacity: "0"
+        },
+        to: {
+            opacity: "1"
+        }
+    },
+
+      "puff-in-center": {
+        "0%": {
+            transform: "scale(2)",
+            filter: "blur(2px)",
+            opacity: "0"
+        },
+        to: {
+            transform: "scale(1)",
+            filter: "blur(0)",
+            opacity: "1"
+        }
+    },
+
+
+      "fade-in-bottom": {
+        "0%": {
+            transform: "translateY(50px)",
+            opacity: "0"
+        },
+        to: {
+            transform: "translateY(0)",
+            opacity: "1"
+        }
+    },
+
+      "nothing": {
+        "0%": {
+            opacity: "100%"
+        },
+        to: {
+          opacity: "100%"
+        }
+      },
+
+
+      "highlight": {
+
+        "0%": {
+          
+          transform: "scaleX(1)"
+        },
+        to: {
+          transform: "scaleX(0)"
+        }
+
+
+      },
+
+      "scale-in-left": {
+        "0%": {
+            transform: "scale(0)",
+            "transform-origin": "0% 50%",
+            opacity: "1"
+        },
+        to: {
+            transform: "scale(1)",
+            "transform-origin": "0% 50%",
+            opacity: "1"
+        }
+    },
+
+
+
+      "rotate-emoji": {
+        "0%": {
+          transform: "rotate(0deg)"
+        },
+        "100%": {
+          transform: "rotate(360deg)"
+        }
+    },
+    "pulsate-fwd": {
+      "0%,to": {
+          transform: "scale(0.8)"
+      },
+      "50%": {
+          transform: "scale(1.2)"
+      }
+  },
+
+      "vibrate-1": {
+        "0%,to": {
+            transform: "translate(0)"
+        },
+        "20%": {
+            transform: "translate(-2px, 2px)"
+        },
+        "40%": {
+            transform: "translate(-2px, -2px)"
+        },
+        "60%": {
+            transform: "translate(2px, 2px)"
+        },
+        "80%": {
+            transform: "translate(2px, -2px)"
+        }
+    },
+
+
+
+      heartbeat: {
+        "0%": {
+            transform: "scale(1.3)",
+            "transform-origin": "center center",
+            "animation-timing-function": "ease-out"
+        },
+        "10%": {
+            transform: "scale(.96)",
+            "animation-timing-function": "ease-in"
+        },
+        "17%": {
+            transform: "scale(1.1)",
+            "animation-timing-function": "ease-out"
+        },
+        "33%": {
+            transform: "scale(.9)",
+            "animation-timing-function": "ease-in"
+        },
+        "45%": {
+            transform: "scale(1.3)",
+            "animation-timing-function": "ease-out"
+        }
+    },
+
+
+
+
+      "text": {
+        "0%": {
+          "letter-spacing": "-.5em",
+          filter: "blur(12px)",
+          opacity: "0",
+          "position": "absolute",
+      },
+      "5%": {
+        "letter-spacing": "0em",
+        filter: "blur(0)",
+        opacity: "1",
+        display:"inline",
+        "position": "absolute",
+    },
+    
+    "25%": {
+      "letter-spacing": "0em",
+      filter: "blur(0)",
+      opacity: "1",
+        display:"inline",
+        "position": "absolute",
+        
+    },
+    "37%": {
+      "letter-spacing": "-.5em",
+    filter: "blur(12px)",
+    opacity: "0",
+    "position": "absolute",
+    },
+      "100%": {
+        "letter-spacing": "-.5em",
+    filter: "blur(12px)",
+    opacity: "0",
+    "position": "absolute",
+      }
+    },
+    
+
+
+
+
+    "emoji": {
+      "0%": {
+      
+        opacity: "0",
+        "position": "absolute",
+        "visible": "none"
+      },
+      "1%": {
+      
+        opacity: "0",
+        "position": "static",
+      },
+    "5%": {
+      opacity: "1",
+      "position": "static",
+  },
+  
+  "25%": {
+    opacity: "1",
+      "position": "static",
+      
+  },
+  "30%": {
+    
+    opacity: "0",
+    "position": "static",
+    
+  },
+  "37%": {
+    
+    opacity: "0",
+    "position": "absolute",
+    "visible": "none"
+  },
+    "100%": {
+      
+      opacity: "0",
+      "position": "absolute",
+      "visible": "none"
     }
+  },
+
+
+
+
+
+    "second": {
+      "0%": {
+        "letter-spacing": "-.5em",
+        filter: "blur(12px)",
+        opacity: "0",
+        "position": "absolute",
+    },
+    
+  "28%": {
+    "letter-spacing": "-.5em",
+    filter: "blur(12px)",
+    opacity: "0",
+    "position": "absolute",
+  },
+  "33%": {
+    "letter-spacing": "0em",
+    filter: "blur(0)",
+    opacity: "1",
+    "position": "static",
+  },
+  "51%": {
+    "letter-spacing": "0em",
+    filter: "blur(0)",
+    opacity: "1",
+    "position": "static",
+  },
+  "61%": {
+    "letter-spacing": "-.5em",
+    filter: "blur(12px)",
+    opacity: "0",
+    "position": "absolute",
+  },
+    "100%": {
+      "letter-spacing": "-.5em",
+      filter: "blur(12px)",
+      opacity: "0",
+      "position": "absolute",
+    }
+  },
+
+  "third": {
+    "0%": {
+      "letter-spacing": "-.5em",
+      filter: "blur(12px)",
+      opacity: "0",
+      "position": "absolute",
+  },
+  
+"56%": {
+  "letter-spacing": "-.5em",
+  filter: "blur(12px)",
+  opacity: "0",
+  "position": "absolute",
+},
+"61%": {
+  "letter-spacing": "0em",
+  filter: "blur(0)",
+  opacity: "1",
+  "position": "static",
+},
+"79%": {
+  "letter-spacing": "0em",
+  filter: "blur(0)",
+  opacity: "1",
+  "position": "static",
+},
+"100%": {
+  "letter-spacing": "-.5em",
+  filter: "blur(12px)",
+  opacity: "0",
+  "position": "absolute",
+},
+},
+    
+
+
+
+      "slide-in-blurred-left": {
+        "0%": {
+            transform: "translateX(-1000px) scaleX(2.5) scaleY(.2)",
+            "transform-origin": "100% 50%",
+            filter: "blur(40px)",
+            opacity: "0"
+        },
+        to: {
+            transform: "translateX(0) scaleY(1) scaleX(1)",
+            "transform-origin": "50% 50%",
+            filter: "blur(0)",
+            opacity: "1"
+        }
+    },
+      pulse: {
+        "0%, 100%": { opacity: 1 },
+        "50%": { opacity: 0.4 },
+      },
+      a: {
+        "0%, 100%": { opacity: 1 },
+        "50%": { opacity: 0.4 },
+      }
+    },
+
+
     },
   },
   plugins: [require("@tailwindcss/forms")],
